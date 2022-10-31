4b583839737351df510fd1069a1c126853024801
Codehash001 <codehashblockchain@gmail.com>
2022-10-31 15:20:44 +0530

    update 5.4

--- components/Main.js
+++ components/Main.js
@@ -15,17 +15,14 @@ export default function Main() {
             make your <br/>own Story in <br/>DEXBATTALE
           </div>
           <div className='flex flex-row items-center'>
-          <div className='w-70 h-auto'>
-          <p><a href="https://commons.wikimedia.org/wiki/File:Google_Play_Store_badge_EN.svg#/media/File:Google_Play_Store_badge_EN.svg">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/1200px-Google_Play_Store_badge_EN.svg.png" alt="Google Play Store badge EN.svg"/></a>
-            </p>
+          <div className=''>
+          <a href='/'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/1200px-Google_Play_Store_badge_EN.svg.png" alt="Google Play Store badge EN.svg'
+            className=' h-[50px] w-auto'/></a>
           </div>
 
-          <div className=' ml-10 w-70 h-auto'>
-          <p><a href="https://commons.wikimedia.org/wiki/File:Download_on_the_App_Store_Badge.svg#/media/File:Download_on_the_App_Store_Badge.svg">
-            <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/1200px-Download_on_the_App_Store_Badge.svg.png" alt="Download on the App Store Badge.svg"/></a>
-          
-          </p>
+          <div className=' ml-10'>
+            <a href='/'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/1200px-Download_on_the_App_Store_Badge.svg.png'
+            className=' h-[50px] w-auto'/> </a>
           </div>
           </div>
 
