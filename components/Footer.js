0c46c7f2e14a68f0dafa7d16882263546a464560
Codehash001 <codehashblockchain@gmail.com>
2022-10-31 20:29:26 +0530

    updfate 8.2

--- components/Footer.js
+++ components/Footer.js
@@ -7,10 +7,10 @@ function Footer () {
       <div className='flex md:justify-evenly items-center justify-between w-full h-20 px-2 2xl:px-16 bg-black my-4'>
 	
             <a href="" className='md:Text-2xl text-1xl text-gray-600' target="_blank" rel="noreferrer">Mint</a>
-            <a href="https://twitter.com/PepeY00tsYC" className='md:Text-2xl text-1xl text-gray-600' target="_blank" rel="noreferrer">Twitter</a>
+            <a href="https://twitter.com/" className='md:Text-2xl text-1xl text-gray-600' target="_blank" rel="noreferrer">Twitter</a>
 	
 	
-            <Link href="/"><Image src="/Dex.png" height='80px' width="80px" className='cursor-pointer'/></Link>
+            <Link href="/"><img src="/Dex.png" className='cursor-pointer h-[60px] w-[80px]'/></Link>
 	
             <a href="https://etherscan.io/address/0xaC8cB0A8642a9cb8461DD74C3799c4865Db39348#readContract" className='md:Text-2xl text-1xl text-gray-600' target="_blank" rel="noreferrer">BscScan</a>
             <a href="https://discord.gg" className='md:Text-2xl text-1xl text-gray-600' target="_blank" rel="noreferrer">Discord</a>
--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,9 @@ export default function Home() {
       <Nfts/>
       <Coins/>
       <Roadmap/>
-      <Footer/>
       <Team/>
+      <Footer/>
+      
        
     
     
