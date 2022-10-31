498ed18ea1c7928b98664f09539afa7315f03fd5
Codehash001 <codehashblockchain@gmail.com>
2022-10-31 13:50:12 +0530

    commit 3

--- components/Navbar.js
+++ components/Navbar.js
@@ -21,18 +21,18 @@ function Navbar () {
           />
         </li>
           <div className='bg-gray-700/30 py-2 px-4 backdrop-blur-md flex flex-row font-Kanit text-white rounded-md text-[20px]'>
-            <Link className="cursor-pointer" acticeClass ="active" to='home' spy={true} smooth={true} offset={-70} duration={500}><h1 className='mx-4'> Home</h1></Link>
-            <Link className="cursor-pointer" acticeClass ="active" to='nfts' spy={true} smooth={true} offset={-70} duration={500}><h1 className='mx-4'> NFTs</h1></Link>
-            <Link className="cursor-pointer" acticeClass ="active" to='dexcoin' spy={true} smooth={true} offset={-70} duration={500}><h1 className='mx-4'> Dex Coin</h1></Link>
-            <Link className="cursor-pointer" acticeClass ="active" to='roadmap' spy={true} smooth={true} offset={-70} duration={500}><h1 className='mx-4'> Roadmap</h1></Link>
-            <Link className="cursor-pointer" acticeClass ="active" to='/' spy={true} smooth={true} offset={-70} duration={500}><h1 className='mx-4'> About</h1></Link>
-            <Link className="cursor-pointer" acticeClass ="active" to='team' spy={true} smooth={true} offset={-70} duration={500}><h1 className='mx-4'> Team</h1></Link>
+            <Link className="cursor-pointer hover:text-blue-400" acticeClass ="active" to='home' spy={true} smooth={true} offset={-70} duration={500}><h1 className='mx-4'> Home</h1></Link>
+            <Link className="cursor-pointer hover:text-blue-400" acticeClass ="active" to='nfts' spy={true} smooth={true} offset={-70} duration={500}><h1 className='mx-4'> NFTs</h1></Link>
+            <Link className="cursor-pointer hover:text-blue-400" acticeClass ="active" to='dexcoin' spy={true} smooth={true} offset={-70} duration={500}><h1 className='mx-4'> Dex Coin</h1></Link>
+            <Link className="cursor-pointer hover:text-blue-400" acticeClass ="active" to='roadmap' spy={true} smooth={true} offset={-70} duration={500}><h1 className='mx-4'> Roadmap</h1></Link>
+            <Link className="cursor-pointer hover:text-blue-400" acticeClass ="active" to='team' spy={true} smooth={true} offset={-70} duration={500}><h1 className='mx-4'> Team</h1></Link>
+            <Link className="cursor-pointer hover:text-blue-400" acticeClass ="active" to='/' spy={true} smooth={true} offset={-70} duration={500}><h1 className='mx-4'> About</h1></Link>
 
           </div>
 
         <li>
-        <div className=' py-2 px-4 backdrop-blur-md flex flex-row font-Kanit text-white rounded-md border border-white'>
-            <h1 className='mx-4 uppercase'>Learn More</h1>
+        <div className=' py-2 px-4 backdrop-blur-md flex flex-row font-Kanit text-white rounded-md border border-white hover:bg-blue-400'>
+            <a href='/mint'><h1 className='mx-4 uppercase tracking-wide hover:font-semibold'>MINT NOW</h1> </a>
 
           </div>
           
