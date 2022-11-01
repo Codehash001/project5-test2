a7c6dddb5008c1a96a475465708ad7e898cc7208
Codehash001 <codehashblockchain@gmail.com>
2022-11-01 19:20:08 +0530

    aos update 2.0

--- components/NFTs.js
+++ components/NFTs.js
@@ -7,7 +7,7 @@ import "aos/dist/aos.css";
 function Nfts () {
 
     useEffect(() => {
-        Aos.init({ duration : 1000,
+        Aos.init({ duration : 1500,
                    delay : 100})
       }, []);
 
@@ -19,16 +19,16 @@ function Nfts () {
        src='/DexBattle PNG/DexBattle-Characters.jpg'
        className='object-cover'/>
        <div className='mt-10'>
-        <h1 data-aos="flip-up" className='text-3xl text-blue-400  font-Kanit text-center uppercase font-semibold tracking-wide'>The Problem</h1>
-        <p data-aos="flip-up" className='text-center font-Kanit text-[20px] text-gray-400 mt-4 mb-5'>
+        <h1 data-aos="flip-right" className='text-3xl text-blue-400  font-Kanit text-center uppercase font-semibold tracking-wide'>The Problem</h1>
+        <p data-aos="flip-right" className='text-center font-Kanit text-[20px] text-gray-400 mt-4 mb-5'>
         Dex battle game was created because there are far too many blockchain games that lack good visuals, and the PC
 gaming area is mostly disregarded by P2E producers. Furthermore, traditional games do not provide their players with
 a play-to-earn structure. Over 1 billion players have been playing games for years without making any money from
 them. They spend hundreds of dollars on skins and other <br/>
 in-game things, yet they cannot sell them. This represents a return on investment.
             </p>
-            <h1 data-aos="flip-up" className='text-3xl text-blue-400  font-Kanit text-center uppercase mt-10 font-semibold tracking-wide'>Our Solution</h1>
-        <p  data-aos="flip-up" className='text-center font-Kanit text-[20px] text-gray-400 mt-4 mb-5'>
+            <h1 data-aos="flip-right" className='text-3xl text-blue-400  font-Kanit text-center uppercase mt-10 font-semibold tracking-wide'>Our Solution</h1>
+        <p  data-aos="flip-right" className='text-center font-Kanit text-[20px] text-gray-400 mt-4 mb-5'>
         We want to create a play-to-earn iOS and Android game with stunning visuals and gameplay
 to attract a share of the 1 billion conventional Android gamers. We also hope to supply our
 gamers with an NFT ecosystem in order to establish a long-lasting and addictive P2E gaming
@@ -43,14 +43,14 @@ experience.
        className='object-cover mt-10 rounded-lg border border-gray-400'/>
 
 <div className='mt-10'>
-        <h1 data-aos="flip-up" className='text-3xl text-blue-400 font-Kanit text-center uppercase font-semibold tracking-wide'>NFT Characters</h1>
-        <p data-aos="flip-up" className='text-center font-Kanit text-[20px] text-gray-400 mt-4'>
+        <h1 data-aos="flip-right" className='text-3xl text-blue-400 font-Kanit text-center uppercase font-semibold tracking-wide'>NFT Characters</h1>
+        <p data-aos="flip-right" className='text-center font-Kanit text-[20px] text-gray-400 mt-4'>
         Every character in the Dex battle P2E game is an NFT asset that can be enhanced using gemstones, and in-game currency (Dex
 Token). Players will utilize their characters to complete objectives, win fights, and gain prizes in many game formats, including
 guilds systems.
             </p>
-            <h1 data-aos="flip-up" className='text-3xl text-blue-400 font-Kanit text-center uppercase mt-10 font-semibold tracking-wide'>NFT marketplace</h1>
-        <p data-aos="flip-up" className='text-center font-Kanit text-[20px] text-gray-400 mt-4'>
+            <h1 data-aos="flip-right" className='text-3xl text-blue-400 font-Kanit text-center uppercase mt-10 font-semibold tracking-wide'>NFT marketplace</h1>
+        <p data-aos="flip-right" className='text-center font-Kanit text-[20px] text-gray-400 mt-4'>
         NFT market place will be created with whitelisting, presale and public sales funtionality to buy the NFTs with your token only and auction and lottery
 too. Dex coin features blockchain-based digital assets know as &ldquo;NFTs&ldquo; that operate on an eco-friendly proof-of-stake blockchain. Because blockchain
 technology enables actual digital ownership, indira characters may be exchanged not just on the specific indira marketplace, but also on a variety of
