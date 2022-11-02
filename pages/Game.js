dcc8e801728719364ae74d1b7926577db2581f0d
Codehash001 <codehashblockchain@gmail.com>
2022-11-02 15:07:41 +0530

    12.9

--- pages/Game.js
+++ pages/Game.js
@@ -101,7 +101,7 @@ function Coins () {
                     </h1>
                 </div>
 
-                <div>
+                <div className='mb-4'>
                     <button className=' border border-blue-200 text-center text-[20px] text-blue-200 font-Kanit hover:bg-blue-400 hover:font-semibold px-4 py-2 rounded-full'>
                         Read more in Whitepaper
                     </button>
@@ -112,14 +112,66 @@ function Coins () {
                 </h1>
 
     
-  <div class="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
+  <div class="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 w-full my-8">
     <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
       <div class="h-96 w-72">
         <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src="/characters/Alita.png" alt="" />
       </div>
       <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
       <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-        <h1 class="font-Kanit text-3xl font-bold text-white uppercase">BISHOP</h1>
+        <h1 class="font-Kanit text-3xl font-bold text-white uppercase">APOLLO</h1>
+        <ol class="mb-3 text-[15px] italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+        <li>Walkspeed:4.5</li>
+        <li>Runspeed:8</li>
+        <li>Stealthspeed:1</li>
+        <li>Acceleration:9</li>
+        <li>Crouchspeed:2</li>
+        <li>Crouchtransitionspeed:0.16</li>
+        <li>Slidespeed:12</li>
+        <li>Jumpforce:8.5</li>
+        <li>Jumprate:0.82</li>
+        <li>Gravitymultiplier:1</li>
+        <li>Health:100</li>
+        <li>Start toregeneratein:4</li>
+        <li>Regenerationspeed:3(max)</li>
+        <li>Regenerateupto:75</li>
+        </ol>
+    </div>
+    </div>
+
+    <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+      <div class="h-96 w-72">
+        <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src="/characters/Alita.png" alt="" />
+      </div>
+      <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+      <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+        <h1 class="font-Kanit text-3xl font-bold text-white">BILLY</h1>
+        <ol class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+        <li>Walkspeed:6.62</li>
+        <li>Runspeed:11.13</li>
+        <li>Stealthspeed:1.16</li>
+        <li>Acceleration:7.4</li>
+        <li>Crouchspeed:1.77</li>
+        <li>Crouchtransitionspeed:0.16</li>
+        <li>Slidespeed:11</li>
+        <li>Jumpforce:8.2</li>
+        <li>Jumprate:0.82</li>
+        <li>Gravitymultiplier:2</li>
+        <li>Health:100</li>
+        <li>Start regeneratein:4</li>
+        <li>Regenerationspeed:3(max)</li>
+        <li>Regenerateupto:75</li>
+        </ol>
+        
+      </div>
+    </div>
+    <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
+      <div class="h-96 w-72">
+        <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src="/characters/Angela.png" alt="" />
+      </div>
+      <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+      <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+        <h1 class="font-Kanit text-3xl font-bold text-white">BISHOP</h1>
         <ol class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
             <li>Walkspeed:5 </li>
             <li>Runspeed:7.85</li>
@@ -128,30 +180,15 @@ function Coins () {
             <li>Crouchspeed:2</li>
             <li>Crouchtransitionspeed:0.16</li>
             <li>Slidespeed:12</li>
+            <li>Jumpforce:5</li>
+            <li>Jumprate:0.82</li>
+            <li>Gravitymultiplier:2</li>
+            <li>Health:100</li>
+            <li>Start toregeneratein:4</li>
+            <li>Regenerationspeed:3(max)</li>
+            <li>Regenerateupto:75</li>
         </ol>
-    </div>
-    </div>
-
-    <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-      <div class="h-96 w-72">
-        <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src="https://images.unsplash.com/photo-1494145904049-0dca59b4bbad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" alt="" />
-      </div>
-      <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-      <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-        <h1 class="font-Kanit text-3xl font-bold text-white">Beyond Builder</h1>
-        <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
-        <button class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See More</button>
-      </div>
-    </div>
-    <div class="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-      <div class="h-96 w-72">
-        <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src="https://images.unsplash.com/photo-1502675135487-e971002a6adb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" alt="" />
-      </div>
-      <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-      <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-        <h1 class="font-Kanit text-3xl font-bold text-white">Shooting Star</h1>
-        <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolore adipisci placeat.</p>
-        <button class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">See More</button>
+        
       </div>
     </div>
   </div>
