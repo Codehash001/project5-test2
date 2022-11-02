f76f163ab458e7cdb59946e8d368c0b1dbce21d2
Codehash001 <codehashblockchain@gmail.com>
2022-11-02 13:32:32 +0530

    12.6

--- pages/Game.js
+++ pages/Game.js
@@ -10,7 +10,7 @@ function Coins () {
 
     useEffect(() => {
         Aos.init({ duration : 1500,
-                   offset: 130,
+                   offset: 100,
                    delay : 500})
       }, []);
       
@@ -18,10 +18,10 @@ function Coins () {
         <div id="game"className='overflow-hidden flex flex-col'>
             <div className='mx-16 flex flex-col items-center justify-between'>
 
-            <h1 data-aos="flip-right" className='text-[40px] font-Kanit text-blue-400 tracking-wide uppercase my-[20px]'>
+            <h1 data-aos="fade" className='text-[40px] font-Kanit text-blue-400 tracking-wide uppercase my-[20px]'>
                     Game Concept
                 </h1>
-                <p data-aos="flip-right" className='text-[20px] font-Kanit text-gray-400 text-justify '>
+                <p data-aos="fade" className='text-[20px] font-Kanit text-gray-400 text-justify '>
                 The game is inspired by a survival game in which players will take on the form of heroes to be the last survivor to win the battle by
                 following the quest. The game loop will be players experiencing the battles, being the winner, and receiving the rewards, then using the
                 rewards to upgrade the game characters and weapons efficiently
@@ -30,8 +30,8 @@ function Coins () {
                 <h1 data-aos="flip-right" className='text-[40px] font-Kanit text-blue-400 tracking-wide uppercase my-[20px]'>
                     Game story
                 </h1>
-                <div className=' md:flex-row flex flex-col justify-between'>
-                    <h1 data-aos="flip-right" className='text-[20px] font-Kanit text-gray-400 text-justify mr-8 '>
+                <div data-aos="fade-up" className=' md:flex-row flex flex-col justify-between'>
+                    <h1  className='text-[20px] font-Kanit text-gray-400 text-justify mr-8 '>
                     <span className='font-semibold mb-4 text-[24px] text-yellow-600'>Dr M.G.</span> <br/>
                     He was at the center of the Event, the very catalyst that set off everything. The great start at the center of the
                     Marsten Galaxy held within its power so great that no one, not a single person or creature in the universe, was said
@@ -42,9 +42,9 @@ function Coins () {
                     <img src='/characters/Dr.M.G..png' className='w-[250px] h-[250px] border'/>
                 </div>
 
-                <div className=' md:flex-row flex flex-col justify-between mt-6'>
+                <div data-aos="fade-up" className=' md:flex-row flex flex-col justify-between mt-6'>
                     <img src='/characters/Dr.M.G..png' className='w-[250px] h-[250px] border'/>
-                    <h1 data-aos="flip-right" className='text-[20px] font-Kanit text-gray-400 text-justify ml-8 '>
+                    <h1 className='text-[20px] font-Kanit text-gray-400 text-justify ml-8 '>
                     When he had drilled through, the power that surged out of the center, out through the hole and into space, was
                     greater than he could ever have imagined. Eleven beams, some brighter than others and some even brighter than
                     that, bounced out into the Marsten Galaxy and hit eleven unsuspecting people, granting them the very powers he
@@ -55,8 +55,8 @@ function Coins () {
                     </h1>
                 </div>
 
-                <div className=' md:flex-row flex flex-col justify-between'>
-                    <h1 data-aos="flip-right" className='text-[20px] font-Kanit text-gray-400 text-justify mr-8 '>
+                <div data-aos="fade-up" className=' md:flex-row flex flex-col justify-between mt-8'>
+                    <h1 className='text-[20px] font-Kanit text-gray-400 text-justify mr-8 '>
                     <span className='font-semibold mb-4 text-[24px] text-yellow-600'>No. 1, Angela</span> <br/>
                     When she was a child, she was bred to be a warrior. Born on the planet Expon in the middle of a battlefield, Angela
                     lived her life as normal, thinking that this was all she would ever know. She was the greatest warrior in her group, but
@@ -67,9 +67,9 @@ function Coins () {
                     <img src='/characters/Angela.png' className='w-[250px] h-[250px] border'/>
                 </div>
 
-                <div className=' md:flex-row flex flex-col justify-between mt-6'>
+                <div data-aos="fade-up" className=' md:flex-row flex flex-col justify-between mt-6'>
                     <img src='/characters/Angela.png' className='w-[250px] h-[250px] border'/>
-                    <h1 data-aos="flip-right" className='text-[20px] font-Kanit text-gray-400 text-justify ml-8 '>
+                    <h1 className='text-[20px] font-Kanit text-gray-400 text-justify ml-8 '>
                     The Event knocked her out, and she woke up alone in the middle of a desert, surrounded by the blood of people who
                     didn&apos;t exist anymore. She was a skilled warrior, truly, and had not been hurt in battle, and when the beam hit her it
                     understood that there was no change that needed to be made. She rose to her feet and stayed in her camp alone until
@@ -77,8 +77,32 @@ function Coins () {
                     </h1>
                 </div>
 
+                <div data-aos="fade-up" className=' md:flex-row flex flex-col justify-between mt-8'>
+                    <h1 className='text-[20px] font-Kanit text-gray-400 text-justify mr-8 '>
+                    <span className='font-semibold mb-4 text-[24px] text-yellow-600'>No. 2, Apollo</span> <br/>
+                    Apollo started out as just a young kid, working to make ends meet in the slum area of Planet Perrian. It was a small
+                    planet, made up mostly of industrial areas and factories, and his life was hard and dangerous. He had to work around
+                    machines constantly, and factory accidents were very common. He went to work every day with the knowledge that, at
+                    any moment, something could happen that would kill him. At the moment of the event, he was in the middle of repairs
+                    on an industrial welder, and the force of the blast (and the subsequent beam that hit him square in the chest) knocked
+                    him back, pushing against the welder and searing off half of his face and giving him third degree burns everywhere.  
+                    </h1>
+                    <img src='/characters/Angela.png' className='w-[250px] h-[250px] border'/>
+                </div>
+
+                <div data-aos="fade-up" className=' md:flex-row flex flex-col justify-between mt-6'>
+                    <img src='/characters/Angela.png' className='w-[250px] h-[250px] border'/>
+                    <h1 className='text-[20px] font-Kanit text-gray-400 text-justify ml-8 '>
+                    Because of his proximity to the machines, the great star&apos;s power got confused, and so instead of healing him, it
+                    replaced the burned parts of his body with robot parts, making him bionic. Scared and suddenly completely alone
+                    on his planet (and, unknown to him at the time, only one of twelve people left alive in the entire galaxy), he hid
+                    among buildings and ate the leftovers the others had left behind until Dr M.G. summoned him to him, giving him
+                    the home and the family that he had always so longed for.
+                    </h1>
+                </div>
+
                 <div>
-                    <button className=' bg-blue-200 text-center text-[20px] text-black font-Kanit hover:bg-blue-900 hover:font-semibold px-4 py-2 rounded-xl'>
+                    <button className=' border border-blue-200 text-center text-[20px] text-blue-200 font-Kanit hover:bg-blue-400 hover:font-semibold px-4 py-2 rounded-full'>
                         Read more in Whitepaper
                     </button>
                 </div>
