97d42a52ac957d3f320c47c29b780f56e24d470f
Codehash001 <codehashblockchain@gmail.com>
2022-11-09 17:36:43 +0530

    30

--- components/exchangetokens1.js
+++ components/exchangetokens1.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { useState,useEffect } from "react"
 import { initOnboard } from "../ulits/onboard"
 import { config } from '../dapp.config'
+import Link from 'next/link'
 
 import {
           doBuy,
           doSell,
-          doApprove } from '../ulits/interactex'
+          doApprove,
+        getExchangeRate } from '../ulits/interactex'
 
 export default function Exchange() {
 
@@ -15,6 +17,15 @@ export default function Exchange() {
   const [isMinting, setIsMinting] = useState(false)
   const [onboard, setOnboard] = useState(null)
   const [walletAddress, setWalletAddress] = useState('')
+  const [exchangeRate, setExchangeRate] = useState(0)
+
+
+  useEffect ( () => {
+    const init = async () => {
+      setExchangeRate(await(getExchangeRate()))
+    }
+    init()
+  }, [])
  
 
   useEffect( () => {
@@ -47,26 +58,53 @@ useEffect(() => {
     }
   }
 
+  const [rateArrItems , setRateArrItems] = useState([])
+
+  const addCurrentRate = () =>{
+  setRateArrItems([...rateArrItems,{
+    id:rateArrItems.length,
+    value:1/exchangeRate
+ }])
+ console.log(rateArrItems)
+  }
+
   const doBuyHandler = async () => {
+
     setIsMinting(true)
+    console.log(exchangeRate)
     const payableAmount = payAmount
-    await doBuy(payableAmount) 
+    const {success, status}= await doBuy(payableAmount)
+    setStatus({
+      success,
+      message: status
+    })
+     
 
    
 
     setIsMinting(false)
+    
   }
 
   const doSellHandler = async () => {
     setIsMinting(true)
     const tokenAmountToSell = BigInt(exchangeAmount*10**18)
-    await doApprove(tokenAmountToSell)
-    await doSell(tokenAmountToSell) 
+    const {success} = await doApprove(tokenAmountToSell)
+    if (success) {
+    const {success, status}=await doSell(tokenAmountToSell)
+    setStatus({
+      success,
+      message: status
+    })
+  }
+    
 
    
 
     setIsMinting(false)
   }
+
+
 
   const [exchangeAmount, setExchangeAmount] = useState(0)
 
@@ -74,7 +112,7 @@ useEffect(() => {
     setExchangeAmount (e.target.value)
   }
 
-  const payAmount = (exchangeAmount/config.exchangeRate)
+  const payAmount = exchangeAmount/exchangeRate
 
   const [toggleState, setToggleState] = useState(1);
 
@@ -82,72 +120,14 @@ useEffect(() => {
     setToggleState (index);
    }
 
+return(
+        <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center bg-gradient-to-br from-[#141414] to-[#330042]">
 
+          <div className="w-full h-full flex flex-col items-center justify-center py-2">
+            <img src='/bg2.jpg'
+            className='inset-auto block w-full min-h-screen object-cover'/>
 
+          </div>
 
-    return(
-        <div className='max-h-screen h-full w-full flex flex-col items-center justify-center'>
-            <hl className='mt-10 font-Kanit text-5xl text-white tracking-wide uppercase'>Instant dex coin exchange</hl>
-            <hl className=' mt-2 font-Kanit text-3xl text-gray-300 tracking-wide uppercase'>Swap your BNB with DEX coin</hl>
-            <div className='flex flex-row  my-4 overflow-hidden justify-between mx-[20%]'>
-               <div className='my-4  flex items-start justify-start '>
-                <img src='/dexcoin.jpeg'
-               className='h-[90%] rounded-lg border border-white '/>
-               </div>
-               <div className='border border-white w-full h-full flex flex-col items-center py-4 px-8 rounded-lg my-4 bg-gray-100'>
-                     <h1 className='text-gray-800 font-Kanit text-[22px]'>Connect your Wallet</h1> 
-                    <div className='w-full h-full flex justify-between mt-4 border border-gray-400'>
-                        <button className={toggleState === 1? ' bg-gray-400 text-black text-[18px] font-Kanit py-2 px-10  border-b-4  border-black' : " border  text-gray-400 py-2 text-[18px] font-Kanit px-10 "}
-                        onClick={() => toggleTab(1)}>
-                        BUY </button>
-
-                        <button className={toggleState === 2 ? 'bg-gray-400 text-black text-[18px] font-Kanit py-2 px-10  border-b-4  border-black' : " border  text-gray-400 py-2 text-[18px] font-Kanit px-10 "}
-                        onClick={() => toggleTab(2)}>
-                        SELL </button>
-                    </div> 
-
- {/*buy form*/}
-  <div className='w-full max-w-xs bg-gray-400 rounded-md border'>
-  <form class=" ">
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="dexBlance">
-        Balance
-      </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="dexBalance" type="number" placeholder="0" value={exchangeAmount} onChange={handler}/>
-    </div>
-    <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Balance
-      </label>
-      {/* <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="number" placeholder="0"/> */}
-      <div className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'> {payAmount}</div>
-    </div>
-    <div class="flex items-center justify-between">
-     {walletAddress ? ( <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
-     onClick={toggleState === 1 ? doBuyHandler : doSellHandler}>
-        { toggleState === 1 ? "Buy DEX coins" : "Sell Dex Coins"}
-      </button> ) : ( <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
-      onClick={connectWalletHandler}>
-        Connect Wallet
-      </button> )}  
-      
-
-      
-    </div>
-    <div class="flex items-center justify-between my-4">
-      <button className="w-full bg-gray-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-        Mint NFTs with Dex coin
-      </button>
-      
-    </div>
-  </form>
-  
-</div>
-
-
-               </div>
-
-            </div>
         </div>
     )}
-
