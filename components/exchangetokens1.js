1a5bae8f1423186920bb0235f5ae9340cc3178d9
Codehash001 <codehashblockchain@gmail.com>
2022-11-11 11:42:33 +0530

    31.9

--- components/exchangetokens1.js
+++ components/exchangetokens1.js
@@ -122,7 +122,7 @@ useEffect(() => {
    }
 
 return(
-        <div className="relative min-h-screen h-full w-full overflow-hidden flex flex-col  bg-gradient-to-br from-[#141414] to-[#330042]">
+        <div className="min-h-screen h-full w-full overflow-hidden flex flex-col bg-gradient-to-br from-[#141414] to-[#330042]">
 
             <div className="w-full h-full flex flex-col items-center justify-center py-2">
               
