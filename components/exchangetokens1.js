f3ef4e3402edf4abdde05b33d5910366e0d06d0f
Codehash001 <codehashblockchain@gmail.com>
2022-11-11 12:13:34 +0530

    32.5

--- components/exchangetokens1.js
+++ components/exchangetokens1.js
@@ -4,6 +4,7 @@ import { initOnboard } from "../ulits/onboard"
 import { config } from '../dapp.config'
 import Link from 'next/link'
 import Navbar from '../components/Navbar';
+import Head from 'next/head'
 
 import {
           doBuy,
@@ -124,113 +125,10 @@ useEffect(() => {
 return(
         <div className="min-h-screen h-full w-full overflow-hidden flex flex-col bg-gradient-to-br from-[#141414] to-[#330042]">
 
-            
-              
-
-              <div className="flex flex-col items-center justify-center h-full w-full px-2 md:px-10">
-
-                <div className="z-1 md:max-w-3xl w-full bg-black/75 filter  py-4 rounded-md px-2 md:px-10 flex flex-col items-center
-            bg-gray-800 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-40 border-2 border-gray-100 backdrop-saturate-150">
-                
-                {/*Haeding*/}
-                <h1 className="tracking-wide font-Kanit uppercase font-bold text-3xl md:text-4xl text-brand-02 bg-clip-text mt-4 border-2 border-blue-300 p-3 rounded-md">
-                  Instant Dex Coin Exchange <br/>
-                  <span className='text-[28px]'>Swap your dex coin with BNB</span>
-                </h1>
-
-                {/*main content*/}
-                
-
-        <div className=' flex flex-row items-center justify-between'>
-
-            <div className='flex flex-col items-center mx-8 my-4'>
-
-            
-
-            <div className=' w-[300px] h-[300px] my-4'>
-                <img src='/dexcoin.jpeg'
-                className='object-cover'/>
-            </div>
-
-           
-
-            </div>
-
-            <div className='flex flex-col items-center mx-8 my-4 bg-[#D9CCCC] rounded-md py-4'>
-            <div className='flex flex-col items-center mx-6 '>
-                <h1 className=' mb-4 font-Kanit text-[22px] text-gray-700 '>{walletAddress
-                ? walletAddress.slice(0, 8) + '...' + walletAddress.slice(-4)
-                : 'CONNECT YOUR WALLET'}</h1>
-
-                <div className='w-full max-w-xs bg-[#afaff8] rounded-md border pb-4'>
-                <div className='w-full h-full flex justify-between border border-gray-400'>
-                        <button className={toggleState === 1? ' bg-[#afaff8] text-black text-[18px] font-Kanit py-2 w-full  border-b-4  border-black' : " bg-[#D9CCCC] border text-gray-400 py-2 text-[18px] font-Kanit px-10 "}
-                        onClick={() => toggleTab(1)}>
-                        BUY </button>
-
-                        <button className={toggleState === 2 ? 'bg-[#afaff8] text-black text-[18px] font-Kanit py-2 px-auto w-full border-b-4 border-black' : "bg-[#D9CCCC] border text-gray-400 py-2 text-[18px] font-Kanit px-10 "}
-                        onClick={() => toggleTab(2)}>
-                        SELL </button>
-                    </div> 
-
-                <form class="mx-6">
-                  <div class="my-4">
-                 
-                    <h1 class="block text-gray-700 text-sm font-bold mb-2">
-                      Balance
-                    </h1>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="dexBalance" type="number" value={exchangeAmount} onChange={handler}/>
-                  </div>
-                  <div class="my-4">
-                    <h1 class="block text-gray-700 text-sm font-bold mb-2" >
-                      Balance
-                    </h1>
-                    
-                    
-                    <div className='shadow appearance-none border bg- rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'> {payAmount}</div>
-                  </div>
-                  <div class="flex items-center justify-between">
-                   {walletAddress ? ( <button className="w-full bg-blue-400 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline" type="button"
-                   onClick={toggleState === 1 ? doBuyHandler : doSellHandler }>
-                      { toggleState === 1 ? "Buy DEX coins" : "Sell Dex Coins"}
-                    </button> ) : ( <button className="w-full bg-blue-400 hover:bg-purple-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
-                    onClick={connectWalletHandler}>
-                      Connect Wallet
-                    </button> )}  
-                  </div>
-
-                  {status && (
-              <div
-                className={`border ${
-                  status.success ? 'border-green-500 text-white' : 'border-red-600 text-gray-400'
-                } rounded-md text-start h-full px-4 py-4 w-full mx-auto mt-8 md:mt-4"`}
-              >
-                <p className="flex flex-col space-y-2 text-sm md:text-base break-words ...">
-                  {status.message}
-                </p>
-              </div>
-            )}
-
-
-
-                </form>
-                
-              </div>
-            </div>
-            </div>
-
-
-        </div>
-
-                </div>
-
-              </div>
-
-              <div className="min-h-screen h-full w-full overflow-hidden flex flex-col items-center justify-center bg-brand-background ">
          <Head>
-        <title>The Rainbow Tribe</title>
-        <meta name="Description" content="The rainbow tribe minting dapp!" />
-        <link rel="icon" href="/Logo.png" />
+        <title>Dex Battle Instant Exchange</title>
+        <meta name="Description" content="This is the exchange for buy and sell dex coins with bnb!" />
+        <link rel="icon" href="/dex.png" />
       </Head>
         <div className="relative w-full h-full flex flex-col items-center justify-center py-2">
 
@@ -340,5 +238,5 @@ return(
 
 </div>
 
-        </div>
+        
     )}
