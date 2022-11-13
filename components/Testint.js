4b2067bee1617894460950992a7e4e8dc317531c
Codehash001 <codehashblockchain@gmail.com>
2022-11-13 12:57:02 +0530

    40.7

--- components/Testint.js
+++ components/Testint.js
@@ -124,8 +124,8 @@ useEffect(() => {
 
   return ( 
 
-<div class="min-h-screen bg-gradient-to-tr  from-[#141414] to-[#330042] flex justify-center items-center py-20 mx-4">
-  <div class="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-4 gap-5 space-y-4 md:space-y-2 space-x-2">
+<div class="min-h-screen bg-gradient-to-tr  from-[#141414] to-[#330042] flex justify-center items-center py-20 ">
+  <div class="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-4 gap-5 space-y-4 md:space-y-2 space-x-2 md:mx-4 lg:mx-4">
 
   <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
       <div class="relative">
@@ -146,7 +146,7 @@ useEffect(() => {
         <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">Mint Now</button>
       </div>
     </div>
-
+    
     <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
       <div class="relative">
         <img class="w-full rounded-xl" src="/characters/Regen.png" alt="Regan" />
