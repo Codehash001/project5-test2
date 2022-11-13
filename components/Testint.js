dd9546b2e63711d98884837ceb38888da483dce3
Codehash001 <codehashblockchain@gmail.com>
2022-11-13 11:56:24 +0530

    40.3

--- components/Testint.js
+++ components/Testint.js
@@ -206,6 +206,106 @@ useEffect(() => {
         <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">Mint Now</button>
       </div>
     </div>
+
+    <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+      <div class="relative">
+        <img class="w-full rounded-xl" src="/characters/Regen.png" alt="Regan" />
+        <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">Legendry</p>
+      </div>
+      <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">Regan</h1>
+      <div class="my-4">
+        <div class="flex space-x-1 items-center">
+          <p>Total Supply : 500</p>
+        </div>
+        <div class="flex space-x-1 items-center">
+          <p>Minted : 100</p>
+        </div>
+        <div class="flex space-x-1 items-center">
+          <p>Available : 400</p>
+        </div>
+        <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">Mint Now</button>
+      </div>
+    </div>
+
+    <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+      <div class="relative">
+        <img class="w-full rounded-xl" src="/characters/Regen.png" alt="Regan" />
+        <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">Legendry</p>
+      </div>
+      <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">Regan</h1>
+      <div class="my-4">
+        <div class="flex space-x-1 items-center">
+          <p>Total Supply : 500</p>
+        </div>
+        <div class="flex space-x-1 items-center">
+          <p>Minted : 100</p>
+        </div>
+        <div class="flex space-x-1 items-center">
+          <p>Available : 400</p>
+        </div>
+        <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">Mint Now</button>
+      </div>
+    </div>
+
+    <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+      <div class="relative">
+        <img class="w-full rounded-xl" src="/characters/Regen.png" alt="Regan" />
+        <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">Legendry</p>
+      </div>
+      <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">Regan</h1>
+      <div class="my-4">
+        <div class="flex space-x-1 items-center">
+          <p>Total Supply : 500</p>
+        </div>
+        <div class="flex space-x-1 items-center">
+          <p>Minted : 100</p>
+        </div>
+        <div class="flex space-x-1 items-center">
+          <p>Available : 400</p>
+        </div>
+        <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">Mint Now</button>
+      </div>
+    </div>
+
+    <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+      <div class="relative">
+        <img class="w-full rounded-xl" src="/characters/Regen.png" alt="Regan" />
+        <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">Legendry</p>
+      </div>
+      <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">Regan</h1>
+      <div class="my-4">
+        <div class="flex space-x-1 items-center">
+          <p>Total Supply : 500</p>
+        </div>
+        <div class="flex space-x-1 items-center">
+          <p>Minted : 100</p>
+        </div>
+        <div class="flex space-x-1 items-center">
+          <p>Available : 400</p>
+        </div>
+        <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">Mint Now</button>
+      </div>
+    </div>
+
+    <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+      <div class="relative">
+        <img class="w-full rounded-xl" src="/characters/Regen.png" alt="Regan" />
+        <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">Legendry</p>
+      </div>
+      <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">Regan</h1>
+      <div class="my-4">
+        <div class="flex space-x-1 items-center">
+          <p>Total Supply : 500</p>
+        </div>
+        <div class="flex space-x-1 items-center">
+          <p>Minted : 100</p>
+        </div>
+        <div class="flex space-x-1 items-center">
+          <p>Available : 400</p>
+        </div>
+        <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">Mint Now</button>
+      </div>
+    </div>
     
   </div>
 </div>
