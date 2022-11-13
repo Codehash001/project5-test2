dc6ee6c6f4745b55333db6fa88bded24512f92d2
Codehash001 <codehashblockchain@gmail.com>
2022-11-13 14:48:00 +0530

    42.1

--- components/MintComponent.js
+++ components/MintComponent.js
@@ -24,7 +24,7 @@ function MintComponent() {
               })
               .map((val) => {
                 return(
-                    <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+                    <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500" key={val.id}>
                     <div class="relative">
                       <img class="w-full rounded-xl" src={val.image} alt="Colors" />
                       <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">Legendry</p>
