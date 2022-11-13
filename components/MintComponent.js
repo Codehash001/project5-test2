34824365f8af2570825213693597db5b124e8b0f
Codehash001 <codehashblockchain@gmail.com>
2022-11-13 15:10:43 +0530

    42.4

--- components/MintComponent.js
+++ components/MintComponent.js
@@ -6,11 +6,18 @@ function MintComponent() {
   return (
     <>
       <div class="min-h-screen bg-gradient-to-tr from-red-300 to-yellow-200 flex justify-center items-center py-20 flex flex-col">
-        <div className="w-full h-[60px] bg-white">
-          <input id="searchInput" type="text" placeholder="Search here..." onChange={(event) => {
+      <form>   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
+    <div class="relative">
+        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        </div>
+        <input type="text" id="searchInput" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Character, Wepon..." required
+        onChange={(event) => {
             setSearchTerm(event.target.value);
           }} />
         </div>
+        </form>
         
       <div class="md:px-4 md:grid md:grid-cols-2 lg:grid-cols-4 gap-5 space-y-4 md:space-y-0 lg:mx-4">
           {
@@ -54,5 +61,11 @@ function MintComponent() {
 }
 
 export default MintComponent;
+
+{/* <div className="w-full h-[60px] bg-white">
+          <input id="searchInput" type="text" placeholder="Search here..." onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }} />
+        </div> */}
 
 
