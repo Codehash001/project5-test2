92e1f68790e3e3b4808c6e0af1e59757dda1fc7c
Codehash001 <codehashblockchain@gmail.com>
2022-11-14 14:13:46 +0530

    45

--- components/Testint.js
+++ components/Testint.js
@@ -143,7 +143,7 @@ useEffect(() => {
             <div class="flex space-x-1 items-center">
               <p>Available:400</p>
             </div>
-            <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">Buy Lesson</button>
+            <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg hover:bg-purple-700">Mint now</button>
           </div>
         </div>
 
@@ -163,7 +163,7 @@ useEffect(() => {
             <div class="flex space-x-1 items-center">
               <p>Available:400</p>
             </div>
-            <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">Buy Lesson</button>
+            <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg hover:bg-purple-700">Mint now</button>
           </div>
         </div>
 
@@ -183,7 +183,27 @@ useEffect(() => {
             <div class="flex space-x-1 items-center">
               <p>Available:400</p>
             </div>
-            <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg">Buy Lesson</button>
+            <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg hover:bg-purple-700">Mint now</button>
+          </div>
+        </div>
+
+        <div class="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
+          <div class="relative">
+            <img class="w-full rounded-xl" src="/characters/Regen.png" alt="Colors" />
+            <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">Legendry</p>
+          </div>
+          <h1 class="mt-4 text-gray-800 text-2xl font-bold cursor-pointer">Regan</h1>
+          <div class="my-4">
+            <div class="flex space-x-1 items-center">
+              <p>Supply:500</p>
+            </div>
+            <div class="flex space-x-1 items-center">
+              <p>Minted:100</p>
+            </div>
+            <div class="flex space-x-1 items-center">
+              <p>Available:400</p>
+            </div>
+            <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg hover:bg-purple-700">Mint now</button>
           </div>
         </div>
 
