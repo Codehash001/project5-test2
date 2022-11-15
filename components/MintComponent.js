70512b3536f76463eb087c3ff91d39b84dcc336e
Codehash001 <codehashblockchain@gmail.com>
2022-11-15 11:21:34 +0530

    49

--- components/MintComponent.js
+++ components/MintComponent.js
@@ -62,7 +62,7 @@ export default function Mint(){
       setMaxUncommonSupply(await getMaxUncommonSupply())
       setMaxCommonSupply(await getMaxCommonSupply())
 
-      setTotalMinted(await getTotalMinted(TokenId))
+      //setTotalMinted(await getTotalMinted(TokenId))
 
       setPaused(await isPausedState())
       //setIsPublicSale(await isPublicSaleState())
@@ -116,6 +116,12 @@ useEffect(() => {
     })
 
     setIsMinting(false)
+  }
+
+  const MintedAmount = async () => {
+    const minted= await getTotalMinted(TokenId)
+    return minted
+    
   }
    
 
@@ -171,7 +177,7 @@ useEffect(() => {
                       <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg hover:bg-purple-700"
                       onClick={() => setTokenId(val.id)}
                       >Mint now</button>
-                      <button text-xl onClick={console.log(TokenId)}>Show Now</button>
+                      <button text-xl onClick={console.log(MintedAmount)}>Show Now</button>
                     </div>
                   </div>
           
