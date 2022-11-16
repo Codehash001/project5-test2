8f83f788b25f35b6671b1d3058375abe62fd27d1
Codehash001 <codehashblockchain@gmail.com>
2022-11-16 14:15:52 +0530

    53.4

--- components/Amzon.js
+++ components/Amzon.js
@@ -4,7 +4,7 @@ import Cards from "./card";
 
 const Amazon = () => {
     const handleClick = (item) => {
-        console.log(item);
+        console.log(item.id);
     };
   return (
     <section className="min-h-screen bg-gradient-to-tr from-[#141414] to-[#330042] justify-center items-center py-10 flex flex-col overflow-hidden">
