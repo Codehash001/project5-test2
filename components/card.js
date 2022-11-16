72c6472c29bdf1d3903c751fa007c443f670285e
Codehash001 <codehashblockchain@gmail.com>
2022-11-16 20:24:09 +0530

    55.3

--- components/card.js
+++ components/card.js
@@ -1,17 +1,31 @@
 import { useEffect, useState } from "react";
 import React from "react";
 import {
-    getTotalMinted          } from '../ulits/interact'
+    getTotalMinted,
+    getMaxLeaderSupply,
+    getMaxLegendarySupply,
+    getMaxEpicSupply,
+    getMaxRareSupply,
+    getMaxUncommonSupply,
+    getMaxCommonSupply,
+    isPausedState          } from '../ulits/interact'
 
 const Cards = ({ item, handleClick, }) => {
 
 const [totalMinted, setTotalMinted ] = useState(0)
+const [maxLeaderSupply, setMaxLeaderSupply] = useState(0)
+const [maxLegendarySupply, setMaxLegendarySupply] = useState(0)
+const [maxEpicSupply, setMaxEpicSupply] = useState(0)
+const [maxRareSupply, setMaxRareSupply] = useState(0)
+const [maxUncommonSupply, setMaxUncommonSupply] = useState(0)
+const [maxCommonSupply, setMaxCommonSupply] = useState(0)
+const [maxSupply, setMaxSupply] = useState(0)
 
 
 
  const { id ,title, tier, supply, image } = item;
  
- useEffect(() => {
+useEffect(() => {
     const init = async (id) => {
       
       setTotalMinted(await getTotalMinted(id))
@@ -19,6 +33,25 @@ const [totalMinted, setTotalMinted ] = useState(0)
 
     init(id)
   }, [])
+
+useEffect(() => {
+    const init = async (id) => {
+      
+        setMaxLeaderSupply(await getMaxLeaderSupply())
+        setMaxLegendarySupply(await getMaxLegendarySupply())
+        setMaxEpicSupply(await getMaxEpicSupply())
+        setMaxRareSupply(await getMaxRareSupply())
+        setMaxUncommonSupply(await getMaxUncommonSupply())
+        setMaxCommonSupply(await getMaxCommonSupply())
+  
+        setPaused(await isPausedState())
+        setMaxSupply(id >= 128 ? maxLegendarySupply : id >= 99 ? maxEpicSupply : id >= 64 ? maxRareSupply : id >= 37 ? maxUncommonSupply : id >= 10 ? maxCommonSupply : maxLeaderSupply)
+
+ }
+
+    init(id)
+  }, [])
+ 
   return (
     <div class="max-w-sm bg-gray-200 px-6 pt-6 pb-2 rounded-xl shadow-lg transform hover:scale-105 transition duration-500">
     <div class="relative">
@@ -29,14 +62,14 @@ const [totalMinted, setTotalMinted ] = useState(0)
     <div class="my-4">
       <div class="flex space-x-1 items-center">
         <p>Supply :
-          {supply}
+        {maxSupply - totalMinted}
           </p>
       </div>
       <div class="flex space-x-1 items-center" >
-        <p> Minted: {totalMinted}</p>
+        <p> Minted: {totalMinted} </p>
       </div>
       <div class="flex space-x-1 items-center">
-        <p>Available:400</p>
+        <p>Available:{totalMinted}</p>
       </div>
        <button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg hover:bg-purple-700"
       onClick={() => handleClick(item)}>
@@ -47,5 +80,3 @@ const [totalMinted, setTotalMinted ] = useState(0)
 };
 
 export default Cards;
-
-// id, title, autor, price, img
