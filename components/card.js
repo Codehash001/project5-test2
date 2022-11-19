e2ca1b16afb00fc6cc7be3f6b676898203299984
Codehash001 <codehashblockchain@gmail.com>
2022-11-20 05:22:36 +0530

    68.2

--- components/card.js
+++ components/card.js
@@ -103,7 +103,7 @@ useEffect(() => {
       <div class="flex space-x-1 items-center">
         <p>Available:{maxSupply - totalMinted}</p>
       </div>
-      {walletAddress?
+      {walletAddress ?
       (<button class="mt-4 text-xl w-full text-white bg-indigo-600 py-2 rounded-xl shadow-lg hover:bg-purple-700"
     onClick={() => handleClick(item)}>
     Mint now</button>) : (
